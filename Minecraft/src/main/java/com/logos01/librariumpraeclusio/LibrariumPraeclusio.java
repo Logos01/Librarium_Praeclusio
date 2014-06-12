ca12bef2af06af550b7e2d225190a5710e19061a
Logos01 <ian.conrad@ex-logos.com>
2014-06-12 14:52:20 -0700

    Added LibrariumPraeclusioCommonProxy

--- Minecraft/src/main/java/com/logos01/librariumpraeclusio/LibrariumPraeclusio.java
+++ Minecraft/src/main/java/com/logos01/librariumpraeclusio/LibrariumPraeclusio.java
@@ -1,9 +1,20 @@
 package com.logos01.librariumpraeclusio;
 
 import net.minecraft.block.Block;
+
 import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.Mod.PreInit;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.network.NetworkMod;
+import cpw.mods.fml.common.network.NetworkRegistry;
+import cpw.mods.fml.common.registry.GameRegistry;
+import cpw.mods.fml.common.registry.LanguageRegistry;
+import cpw.mods.fml.common.network.NetworkMod.SidedPacketHandler;
+import cpw.mods.fml.common.SidedProxy;
+import com.logos01.librariumpraeclusio.LibrariumPraeclusioCommonProxy;
 
 @Mod(modid = LibrariumPraeclusio.MODID, version = LibrariumPraeclusio.VERSION)
 
